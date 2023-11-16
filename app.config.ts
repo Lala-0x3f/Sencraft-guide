1debe2e357767212b50efa156134e6f4bfc9b149
lala0x3f <93862679+Lala-0x3f@users.noreply.github.com>
2023-11-16 17:54:36 +0800

    森：不是Sencraft，是SenCraft

--- app.config.ts
+++ app.config.ts
@@ -1,26 +1,26 @@
 export default defineAppConfig({
   docus: {
-    title: 'Sencraft 教程',
-    description: 'Sencraft 是一个公益的 minecraft 创造服务器。',
+    title: 'SenCraft 教程',
+    description: 'SenCraft 是一个公益的 minecraft 创造服务器。',
     image: '/cover.png',
     socials: {
-      github: 'https://github.com/Lala-0x3f/sencraft-guide',
+      github: 'https://github.com/Lala-0x3f/SenCraft-guide',
       ['qq']: {
         label: 'QQ',
         icon: 'simple-icons:tencentqq',
         href: 'https://pd.qq.com/s/ao0mk068w'
       },
-      ['sencraft 公告板']: {
-        label: 'sencraft 公告板',
+      ['SenCraft 公告板']: {
+        label: 'SenCraft 公告板',
         icon: 'pepicons-pop:bulletin-notice',
-        href: 'https://bbs.sencraft.top/'
+        href: 'https://bbs.SenCraft.top/'
       }
 
     },
     github: {
       dir: '.starters/default/content',
       branch: 'main',
-      repo: 'sencraft-guide',
+      repo: 'SenCraft-guide',
       owner: 'Lala-0x3f',
       edit: true
     },
