5335dd086b5ff77a3254d73b482e8794e2e3412e
Lala-0x3f <ifctmzzz@gmail.com>
2023-11-23 09:53:23 +0800

    add video

---
+++ components/content/CoverVideo.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <video autoplay="false" disable-picture-in-picture="true" preload="auto" controls poster="https://mirror.ghproxy.com/https://github.com/Lala-0x3f/picx-images-hosting/raw/master/20231123/Videoposter.70xa12ni8eg0.webp">
+      <source src="https://jx.91vrchat.com/bl/?url=https://www.bilibili.com/video/BV1zM4y1D7jb/" type="video/mp4">
+    </video>
+  </div>
+</template>
+
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped>
+  div {
+    border-radius: var(--radii-xl);
+    overflow: hidden;
+    border: 1px solid var(--elements-border-primary-static);
+    display: block;
+    position: relative;
+    width: 100%;
+    margin: 3em 0;
+  }
+</style>
+
