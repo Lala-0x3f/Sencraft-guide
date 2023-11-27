37aa460422f6ec96385e8161d9cb0de6e8c96aaa
Lala-0x3f <ifctmzzz@gmail.com>
2023-11-27 16:19:20 +0800

    更新啦

---
+++ components/content/realmsVideo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <video autoplay="false" disable-picture-in-picture="true" preload="auto" controls poster="https://fastly.jsdelivr.net/gh/Lala-0x3f/picx-images-hosting@master/20231127/image.2smeq2avcva0.jpg">
+      <source src="https://jx.91vrchat.com/bl/?url=https://www.bilibili.com/video/BV1M44y1Q7hA/" type="video/mp4">
+    </video>
+  </div>
+</template>
+
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped>
+  div {
+    border-radius: var(--radii-xl);
+    overflow: hidden;
+    border: 1px solid var(--elements-border-primary-static);
+    display: block;
+    position: relative;
+    width: 100%;
+    margin: 3em 0;
+  }
+</style>
