22a8ea7edb2889b464452704a053b4b42556fef4
Lala-0x3f <ifctmzzz@gmail.com>
2023-11-27 17:10:28 +0800

    seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,11 +3,21 @@ export default defineNuxtConfig({
   extends: '@nuxt-themes/docus',
   devtools: { enabled: true },
   css: ['~/assets/style.css'],
-
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      meta: [
+        { name: 'keywords', content: 'Sencraft, SenCraft, 森世界, minecraft, 我的世界, 创造服, 建筑服, 原神, wiki, 百科, 指南, 创世神, help wiki, guide, 插件教程, SC, 下载, minecraft server'},
+        { name: 'twitter:image', content: '/cover.png'},
+      ]
+    }
+  },
   modules: [
     // Remove it if you don't use Plausible analytics
     // https://github.com/nuxt-modules/plausible
-    '@nuxtjs/plausible'
+    '@nuxtjs/plausible',
+    'nuxt-og-image',
   ],
   typescript: {
     tsConfig: {
