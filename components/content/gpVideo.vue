44e678db969104afe445b9456eb11552cde70598
Lala-0x3f <ifctmzzz@gmail.com>
2023-12-02 20:29:49 +0800

    添加gp和gb的视频教程

---
+++ components/content/gpVideo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <video autoplay="false" disable-picture-in-picture="true" preload="auto" controls>
+      <source src="https://jx.91vrchat.com/bl/?url=https://www.bilibili.com/video/BV1kg411h77d/" type="video/mp4">
+    </video>
+  </div>
+</template>
+
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped>
+  div {
+    border-radius: var(--radii-xl);
+    overflow: hidden;
+    border: 1px solid var(--elements-border-primary-static);
+    display: block;
+    position: relative;
+    width: 100%;
+    margin: 3em 0;
+  }
+</style>
