8823cc2aa7c29558ae933e297c970155a787ad5b
lala0x3f <93862679+Lala-0x3f@users.noreply.github.com>
2024-01-29 10:53:04 +0800

    try fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,36 +1,42 @@
 export default defineNuxtConfig({
   // https://github.com/nuxt-themes/docus
-  extends: '@nuxt-themes/docus',
+  extends: "@nuxt-themes/docus",
   devtools: { enabled: true },
-  css: ['~/assets/style.css'],
+  css: ["~/assets/style.css"],
   app: {
     head: {
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
       meta: [
-        { name: 'keywords', content: 'Sencraft, SenCraft, 森世界, minecraft, 我的世界, 创造服, 建筑服, 原神, wiki, 百科, 指南, 创世神, help wiki, guide, 插件教程, SC, 下载, minecraft server'},
-        { name: 'twitter:image', content: '/cover.png'},
-      ]
-    }
+        {
+          name: "keywords",
+          content:
+            "Sencraft, SenCraft, 森世界, minecraft, 我的世界, 创造服, 建筑服, 原神, wiki, 百科, 指南, 创世神, help wiki, guide, 插件教程, SC, 下载, minecraft server",
+        },
+        { name: "twitter:image", content: "/cover.png" },
+      ],
+    },
   },
   modules: [
     // Remove it if you don't use Plausible analytics
     // https://github.com/nuxt-modules/plausible
-    '@nuxtjs/plausible',
-    'nuxt-og-image',
+    "@nuxtjs/plausible",
+    "nuxt-og-image",
   ],
   typescript: {
     tsConfig: {
       compilerOptions: {
-        verbatimModuleSyntax: false
-      }
-    }
+        verbatimModuleSyntax: false,
+      },
+    },
+  },
+  routeRules: {
+    "/**": { ssr: true },
   },
   nitro: {
     prerender: {
-      routes: ['/sitemap.xml']
-    }
+      crawlLinks: true,
+      routes: ["/sitemap.xml"],
+    },
   },
-  ssr: true
-
-})
+});
