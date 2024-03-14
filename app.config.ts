c2acf596448db05bce813c6a72252254c20296d5
lala0x3f <93862679+Lala-0x3f@users.noreply.github.com>
2024-03-14 10:18:17 +0800

    更新  footer

--- app.config.ts
+++ app.config.ts
@@ -39,6 +39,21 @@ export default defineAppConfig({
       exclude: [],
       fluid: true
     },
+    footer: {
+      fluid: true,
+      textLinks: [
+        {
+          text: "萌ICP备20244447号",
+          href: "https://icp.gov.moe/?keyword=20244447",
+          target: "_blank",
+        },
+        {
+          text: "使用 Vercel 部署",
+          href: "https://vercel.com/home",
+          target: "_blank"
+        },
+      ],
+    },
 
   }
 })
