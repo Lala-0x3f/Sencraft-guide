0e84f544b4bf7aea43e57c22b9252c87048a7e32
lala0x3f <93862679+Lala-0x3f@users.noreply.github.com>
2024-04-08 09:35:20 +0800

    Create sitemap2.xml.ts

---
+++ server/routes/sitemap2.xml.ts
@@ -0,0 +1,21 @@
+import { serverQueryContent } from '#content/server'
+import { SitemapStream, streamToPromise } from 'sitemap'
+
+export default defineEventHandler(async (event) => {
+  // Fetch all documents
+  const docs = await serverQueryContent(event).find()
+  const sitemap = new SitemapStream({
+    hostname: 'https://sc.mcbbs.host'
+  })
+
+  for (const doc of docs) {
+    sitemap.write({
+      url: doc._path,
+      changefreq: 'daily'
+      
+    })
+  }
+  sitemap.end()
+
+  return streamToPromise(sitemap)
+})
