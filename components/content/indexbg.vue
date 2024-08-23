14b7e8e724de34886e6abe3a75b146fc5c6b82d2
lala0x3f <93862679+Lala-0x3f@users.noreply.github.com>
2024-08-23 15:41:28 +0800

    更新页面背景
    因为换主城了

--- components/content/indexbg.vue
+++ components/content/indexbg.vue
@@ -15,8 +15,10 @@ export default defineComponent({
     const detectColorMode = (mediaQueryList: MediaQueryList) => {
       // Choose the appropriate background image based on the detected color mode
       backgroundImage.value = mediaQueryList.matches
-        ? 'https://fastly.jsdelivr.net/gh/Lala-0x3f/picx-images-hosting@master/20231128/Screenshot-2023-11-28-162729.3663n6820jw0.webp'
-        : 'https://fastly.jsdelivr.net/gh/Lala-0x3f/picx-images-hosting@master/20231128/Screenshot-2023-11-28-162623.5p2y2c7ejao0.webp';
+        // ? 'https://fastly.jsdelivr.net/gh/Lala-0x3f/picx-images-hosting@master/20231128/Screenshot-2023-11-28-162729.3663n6820jw0.webp'
+        // : 'https://fastly.jsdelivr.net/gh/Lala-0x3f/picx-images-hosting@master/20231128/Screenshot-2023-11-28-162623.5p2y2c7ejao0.webp';
+        ? 'https://i0.hdslb.com/bfs/new_dyn/c89abe82032c09cb186c39f3752f27fd340820855.png@1080w_720h_1e_1c.avif'
+        :   'https://i0.hdslb.com/bfs/new_dyn/0f16d4789b821ca6a4b98063981a7e9e340820855.jpg@1052w_!web-dynamic.avif'
     };
 
     // Call the detectColorMode function when the component is mounted
